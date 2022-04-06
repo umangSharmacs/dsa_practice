22a998858bd218f39319693ce902da2b78758429
umangSharmacs <umangsharma.cs@gmail.com>
2022-04-06 19:59:44 +0530

    h

--- heap.py
+++ heap.py
@@ -1,1 +1,16 @@
-print("hello")
+
+from multiprocessing.sharedctypes import Value
+
+class node:
+    def __init__(self, value) -> None:
+        self.value=value
+        self.left=None
+        self.right=None
+
+class bin_tree:
+
+    def create_bin_tree(self, arr):
+        pass
+
+
+
